04346a8f2888913894fd49ea341747a71b375c76
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-13 13:46:31 +0530

    modified 158a

--- CodeForces/158a.cpp
+++ CodeForces/158a.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
 int main(){
 	int n,k,x,y;
 	cin >> n >> k;
@@ -18,4 +18,23 @@ int main(){
 	}
 	cout << k << '\n';
 
+}
+*/
+int main(){
+	int n,k,x,y;
+	cin >> n >> k;
+	int o = k;
+	for(int i{0};i<k;++i){
+		cin >> x;
+		if (x==0){
+			cout << 0 << '\n';
+			exit(0);
+		}
+	}
+	while(cin >> y){
+		if (y==x){
+			o++;
+		}
+	}
+	cout << o << '\n';
 }
