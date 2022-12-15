009a9d47f56e80c051036a153237bbe26c7ea6e3
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-15 19:42:01 +0530

    added 339a

--- CodeForces/339a.cpp
+++ CodeForces/339a.cpp
@@ -9,10 +9,35 @@ typedef pair<int,int> pi;
 #define PB push_back
 #define MP make_pair
 
+
+bool myCmp(string s1, string s2) {
+if (s1.size() == s2.size()) {
+return s1 < s2;
+}
+else {
+return s1.size() < s2.size();
+}
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    string s,t,d="+";
+    vector<string> l {};
+    getline(cin,s);
+    stringstream x(s);
+    while(getline(x,t,'+')){
+        l.PB(t);
+    }
+    
+    sort(l.begin(),l.end(),myCmp);
+
+    for(size_t i=0;i<l.size()-1;i++){
+        cout << l[i] << "+";
+    }
+
+    cout << l.back() << '\n';
 
 }
 
