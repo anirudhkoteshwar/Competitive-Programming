4d0f8bf99259aad9cfc3473bbd9957f2f985142a
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-15 12:25:03 +0530

    added template.cpp

--- CodeForces/4a.cpp
+++ CodeForces/4a.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+i#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
---
+++ CodeForces/template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vector<int> vi
+#define pair<int,int> pi
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+}
+
+
