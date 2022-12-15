6576510cb6dad8b60680664ebc70e92c5ebed1da
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-15 15:53:57 +0530

    added 263a

--- CodeForces/263a.cpp
+++ CodeForces/263a.cpp
@@ -13,7 +13,20 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-
+    int x;
+    int r=0;
+    int c=0;
+    while(cin >> x){
+        if(x==1){
+            break;
+        }
+        c++;
+        if(c%5==0){
+            r++;
+            c=0;
+        }
+}
+    cout << abs(3-(r+1)) + abs(3-(c+1)) << '\n';
 }
 
 
