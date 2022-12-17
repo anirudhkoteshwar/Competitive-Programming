b372690ef3463c2196fc8147f93d4ab2b277268f
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-12-17 15:00:43 +0530

    added 791a

--- CodeForces/791a.cpp
+++ CodeForces/791a.cpp
@@ -13,7 +13,15 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-
+    int a,b,o=0;
+    cin >> a >> b;
+    for(int i=1;i<100;i++){
+        if(pow(3,i)*a > pow(2,i)*b){
+            o=i;
+            break;
+        }
+    }
+    cout << o << '\n';
 }
 
 
